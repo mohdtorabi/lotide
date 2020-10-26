1e06a8f5e3c9f039ee4a778c6486fc47e81dbef4
mohdtorabi <mitq8.mt@gmail.com>
2020-10-26 22:54:51 +0000

    emojies!

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("🔴 Assertion Passed: " + "[" + actual + "]" + "===" + "[" + expected + "]");
+  } else {
+    console.log("🟢 Assertion Failed: " + "[" + actual + "]" + "!==" + "[" + expected + "]");
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Lighthouse Labs", "Lighthouse Labs");
+assertEqual(1, 2);
