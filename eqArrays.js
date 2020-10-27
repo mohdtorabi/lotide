192ca5185ed079bd5f64b6da6623b16f47039ab9
mohdtorabi <mitq8.mt@gmail.com>
2020-10-27 23:26:10 +0000

    did it with help of my friends here

---
+++ eqArrays.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢 Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`🔴 Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+
+const eqArrays = function(actual, expected) {
+  if (actual.length !== expected.length) {
+    return false;
+  }
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]) {
+      return false;
+    }
+  }
+  return true;
+  
+};
+console.log(eqArrays([1, 2, 3], [1, 2, 3]));
+console.log(eqArrays([1, 2, 3], [3, 2, 1]));
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"]));
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true);
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+
+
