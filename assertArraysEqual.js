f1f30e40904fe4e855ff529ebc0d50f81dc59d54
mohdtorabi <mitq8.mt@gmail.com>
2020-10-27 23:55:53 +0000

    sean helped me!

---
+++ assertArraysEqual.js
@@ -0,0 +1,32 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢 Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`🔴 Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+const eqArrays = function(actual, expected) {
+  if (actual.length !== expected.length) {
+    return false;
+  }
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]) {
+      return false;
+    }
+  }
+  return true;
+  
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`🟢 Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`🔴 Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+
+
+
+
+assertArraysEqual([1, 2, 3],[1, 2, "3"]);
