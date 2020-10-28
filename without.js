c11ba909aca8b35c5d0912e4e4493181336e44f2
mohdtorabi <mitq8.mt@gmail.com>
2020-10-28 00:33:36 +0000

    without you

---
+++ without.js
@@ -0,0 +1,35 @@
+const eqArrays = function(actual, expected) {
+  if (actual.length !== expected.length) {
+    return false;
+  }
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]) {
+      return false;
+    }
+  }
+  return true;
+  
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`🟢 Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`🔴 Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+
+
+
+const without = function (source, itemsToRemove){
+  let result = [];
+  for (i = 0; i < source.length; i++) {
+    if (!itemsToRemove.includes(source[i])) {
+      result.push(source[i]);
+      
+    }
+  }
+  return result;
+};
+console.log(without([1, 2, 3], [1])) // => [2, 3]
+console.log(without(["1", "2", "3"], [1, 2, "3"])) // => ["1", "2"]
