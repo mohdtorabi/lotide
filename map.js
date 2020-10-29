a220af8131dfa989b12202f847f043d23ba8acd2
mohdtorabi <mitq8.mt@gmail.com>
2020-10-29 21:12:40 +0000

    mapping

---
+++ map.js
@@ -0,0 +1,35 @@
+const eqArrays = function(actual, expected) {
+  if (actual.length !== expected.length) {
+    return false;
+  }
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]) {
+      
+      return false;
+    }
+  }
+  return true;
+  
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`🟢 Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`🔴 Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+
+
+const words = ["ground", "control", "to", "major", "tom"];
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+};
+
+const results1 = map(words, word => word[0]);
+assertArraysEqual(results1, ['g','c','t','m','t']);
+
