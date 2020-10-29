b53da55e386949519e636cccb73d8ff2e0b9b81f
mohdtorabi <mitq8.mt@gmail.com>
2020-10-29 22:03:21 +0000

    found the keys

---
+++ findKey.js
@@ -0,0 +1,34 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢🟢🟢 Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+
+
+const findKey = function(objectKey, callBack) {
+  for (let key of Object.keys(objectKey)) {
+    //console.log(objectKey[key], key);
+    if (callBack(objectKey[key])) {
+      return key;
+    }
+  }
+};
+
+const objectFindKey = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2);
+assertEqual(objectFindKey,"noma");
+
+
+
+
+
+
+
