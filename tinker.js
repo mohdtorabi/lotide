c92bbe3380fda183b79c29336fb4408cc5f8d0b6
mohdtorabi <mitq8.mt@gmail.com>
2020-10-31 23:47:31 +0000

    dude i jusr added eqarray to it

--- tinker.js
+++ tinker.js
@@ -23,14 +23,16 @@ const eqArrays = function(actual, expected) {
 };
 
 const eqObjects = function(object1, object2) {
-  
+  if (Object.keys(object1).length !==  Object.keys(object2).length) {
+    return false;
+  }
 
   for (const key of Object.keys(object1)) {
-    
-    if (Object.keys(object1).length !==  Object.keys(object2).length) {
-      return false;
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+      if (!eqArrays(object1[key], object2[key])) {
+        return false;
+      }
     }
-
     if ((object1[key] instanceof Object) &&  (object2[key] instanceof Object)) {
       return eqObjects(object1[key], object2[key]);
     } else {
