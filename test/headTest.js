17b8cff7014349d41fd0883033bd6cc853e8d8dd
mohdtorabi <mitq8.mt@gmail.com>
2020-11-02 20:35:56 +0000

    added everything done so far

--- test/headTest.js
+++ test/headTest.js
@@ -1,6 +1,22 @@
-const headArray = require('../head');
-const assertEqual = require('../assertEqual');
+const head = require('../head');
+const assert = require('chai').assert;
+//const assertEqual = require('../assertEqual');
 
-assertEqual(headArray([5,6,7]), 5);
-assertEqual(headArray(["Hello", "Lighthouse", "Labs"]), "Hello");
+/* assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); */
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+
+  it("returns Hello for [Hello, Lighthouse, Labs] ", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+
+});
 
