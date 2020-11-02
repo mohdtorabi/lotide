897c3fe37f9bd2956d28b669d2fd17f259b781ef
mohdtorabi <mitq8.mt@gmail.com>
2020-11-02 17:27:16 +0000

    tailed

--- tail.js
+++ tail.js
@@ -1,24 +1,14 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🔴 Assertion Passed: [${actual}] === [${expected}]`);
-  } else {
-    console.log(`🟢 Assertion Failed: [${actual}] !== [${expected}]`);
-  }
-};
 
 const tail = function(array) {
-  return array.slice(1);
+  let tailArray = [];
+  for (let i = 1; i < array.length; i++) {
+    tailArray.push(array[i]);
+  }
+  return tailArray;
 };
 
 
 
 
+module.exports = tail;
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-
-assertEqual(words.length, 3);
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs");
